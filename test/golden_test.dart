af63def07b34f1a21fc1489d895ff7ac9aec1e0a
Cristiano Cortez <cristianocortez@gmail.com>
2024-10-17 17:07:23 -0300

    Inserting golden_toolkit

--- test/golden_test.dart
+++ test/golden_test.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_test/main.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
 
 void main() {
   testWidgets('Hello World golden test', (WidgetTester tester) async {
@@ -15,9 +17,15 @@ void main() {
 
     await tester.pumpAndSettle();
 
-    expect(
-      find.text('Hello, world!'),
-      matchesGoldenFile('hello_world.png'),
-    );
+    // expect(
+    //   find.text('Hello, world!'),
+    //   matchesGoldenFile('hello_world.png'),
+    // );
+  });
+
+  testGoldens('MyHomePage Widget golden test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyHomePage(title: 'Golden Test Example'));
+
+    await screenMatchesGolden(tester, 'my_homepage');
   });
 }
